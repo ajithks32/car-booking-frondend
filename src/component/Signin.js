504ecacba2ca9e9a95407eb0776af4172681c6e0
ajithks32 <ajithks5532@gmail.com>
2025-05-06 21:08:03 +0530

    Update Signin.js

--- src/component/Signin.js
+++ src/component/Signin.js
@@ -18,7 +18,7 @@ const Signin = () => {
     const password = event.target.password.value;
   
     try {
-      const response = await axios.post("http://localhost:5000/auth/login", { email, password });
+      const response = await axios.post("https://car-rental-backend-zy09.onrender.com/auth/login", { email, password });
   
       if (response.status === 200) {
         setMessage("Login successful!...");
